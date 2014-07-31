53a06ff2d9f13bca28e589be892d452ee64253e4
zachzoeller <zach.zoeller@gmail.com>
2014-07-30 19:03:04 -0500

    a

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zachzoellerioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zachzoellerioApp
+ */
+angular.module('zachzoellerioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
