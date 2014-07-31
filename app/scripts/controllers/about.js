a334c6efc03e959bfa92c91965064ea668bc529a
zachzoeller <zach.zoeller@gmail.com>
2014-07-31 09:51:44 -0500

    mix branches...

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zachzoellergithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the zachzoellergithubioApp
+ */
+angular.module('zachzoellergithubioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name zachzoellergithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the zachzoellergithubioApp
+ */
+angular.module('zachzoellergithubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
